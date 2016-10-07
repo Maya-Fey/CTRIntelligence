e2d61cf33a49322495b7f3d3448d3ae5facfb397
Claire <foxcoindev@gmail.com>
2016-10-07 16:48:00 +1300

    Fixed naming bug in stats

--- source/source.js
+++ source/source.js
@@ -331,7 +331,7 @@ for(var i = 0; i < raw.length; i++)
 	stats.setAttribute("class", "tagline");
 	stats.setAttribute("style", "color: black");
 	var minsago = (new Date() - new Date(times[i])) / (1000 * 60);
-	stats.innerHTML = "V/C ratio: " + ("" + (scores[i] / comments[i])).substr(0, 4) + " Score gain per hour: " + ("" + (scores[i] / minsago)).substr(0, 4) + " Comments per hour: " + ("" + (comments[i] / minsago)).substr(0, 4);
+	stats.innerHTML = "V/C ratio: " + ("" + (scores[i] / comments[i])).substr(0, 4) + " net gain/min: " + ("" + (scores[i] / minsago)).substr(0, 4) + " comments/min: " + ("" + (comments[i] / minsago)).substr(0, 4);
 	
 	for(var j = 0; j < catnames.length; j++)
 	{
